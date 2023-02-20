dc889b281f7390b6a4348a3db3915b4cbf38dfce
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-20 10:32:07 -0500

    create footer component shell

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './Footer.css'; 
+
+
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <div>Footer Placeholder</div>
+        </footer>
+    )
+
+};
