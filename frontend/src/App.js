fc230ffd56e6e0e0d9213c5a30ab583b09ae71c9
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-20 12:34:57 -0500

    Add routes to App.js; Nest MainPage under SplashPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,22 @@
+import { Switch } from 'react-router-dom';
+// import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import SplashPage from './components/SplashPage/SplashPage';
+import { Route } from 'react-router-dom';
+
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+    <NavBar />
+    <Switch>
+        <Route path="/">
+          <SplashPage />
+        </Route>
+    </Switch>
+    <Footer />
+    </>
   );
 }
 
--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,15 +1,24 @@
 import React from 'react'; 
 import './SplashPage.css'; 
+import MainPage from '../MainPage/MainPage'
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 
 const SplashPage = () => {
+    const loggedIn = useSelector(state => !!state.session.user);
+    const location = useLocation();
 
-    return (
-        <div>
-            SplashPage component container placeholder
-        </div>
-    )
-
+    if (location.pathname === "/" && !loggedIn) {
+        return (
+            <div>
+                Splash Page Container placeholder 
+            </div>
+        )
+    } else {
+        <MainPage />
+    }
 };
 
 export default SplashPage;
