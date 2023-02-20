5d7348a48a993f3b712ef82fbe177f28d9ecd1f3
“Emmett <emmett.wechsler11@gmail.com>
2023-02-20 10:41:10 -0500

    initial setup

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/api/users'); // update the import file path
+
+const app = express();
+
+app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(express.json()); // parse JSON request body
+app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
+app.use(cookieParser()); // parse cookies as an object on req.cookies
+
+// Attach Express routers
+app.use('/api/users', usersRouter); // update the path
+
+module.exports = app;
