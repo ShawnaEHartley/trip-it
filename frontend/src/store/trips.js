89975b2968974a8705849fcc3ff4479c63e2f5b7
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-21 12:15:25 -0500

    Trip reducer file almost done

--- frontend/src/store/trips.js
+++ frontend/src/store/trips.js
@@ -11,27 +11,27 @@ const CLEAR_TRIP_ERRORS = trips/CLEAR_TRIP_ERRORS;
 const receiveTrip = (trip) => ({
     type: RECEIVE_TRIP, 
     trip 
-})
+});
 
 const receiveTrips = (trips) => ({
     type: RECEIVE_TRIPS, 
     trips 
-})
+});
 
 const removeTrip = (tripId) => ({
     type: REMOVE_TRIP, 
     tripId 
-})
+});
 
 const receiveTripErrors = (errors) => ({
     type: RECEIVE_TRIP_ERRORS,
     errors 
-})
+});
 
 const clearTripErrors = (errors) => ({
     type: CLEAR_TRIP_ERRORS,
     errors 
-})
+});
 
 export const fetchTrips = (userId) => async (dispatch) => {
     try {
@@ -57,17 +57,85 @@ export const fetchTrip = (tripId) => async (dispatch) => {
             dispatch(receiveTripErrors(resBody.errors))
         }
     }
+};
+
+
+export const createTrip = (data) = async (dispatch) => {
+    try {
+        const res = await jwtFetch(`/api/trips/`, {
+            method: "POST", 
+            body: JSON.stringify(data)
+        }); 
+        const trip = await res.json(); 
+        dispatch(receiveTrip(trip));
+    } catch(err) {
+        const resBody = await err.json(); 
+        if (resBody.statusCode === 400) {
+            return dispatch(receiveTripErrors(resBody.errors))
+        }
+    }
+};
+
+export const updateTrip = (trip) = async (dispatch) => {
+    try {
+        // const res = await jwtFetch(`/api/trips/`, {
+            method: "PATCH", 
+            body: JSON.stringify(trip)
+        });
+        const trip = await res.json(); 
+        dispatch(receiveTrip(trip));
+    } catch(err) {
+        const resBody = await err.json(); 
+        if (resBody.statusCode === 400) {
+            return dispatch(receiveTripErrors(resBody.errors))
+        }
+    }
+};
+
+export const addMemberToTrip = (tripId, userEmail) = async (dispatch) => {
+
 }
 
-
-const createTrip = (trip) = async (dispatch) => {
-    const res = 
-}
-
-const updateTrip = (tripId) = async (dispatch) => {
+export const removeMemberFromTrip = (tripId, memberId) => {
 
 }
 
-const deleteTrip = (tripId) = async (dispatch) => {
+export const deleteTrip = (tripId) = async (dispatch) => {
+    const res = await fetch(`/api/trips/${tripId}`, {
+        method: "DELETE"
+    })
+    return dispatch(removeTrip(tripId));
+};
 
-}
+
+const nullErrors = null; 
+
+export const tripErrorsReducer = (state = {}, action) => {
+    switch(action.type) {
+        case RECEIVE_TRIP_ERRORS: 
+            return action.errors; 
+        case CLEAR_TRIP_ERRORS: 
+            return nullErrors; 
+        default: 
+            return state; 
+    }
+};  
+
+
+const tripsReducer = (state = {}, action) => {
+    let newState = { ...state }
+
+    switch(action.type) {
+        case RECEIVE_TRIP: 
+            
+        case RECEIVE_TRIPS: 
+            return {...state, ...action.trips}
+        case REMOVE_TRIP: 
+            delete newState[action.trip.tripId]
+            return newState 
+        default: 
+            return newState; 
+    }
+};
+
+export default tripsReducer; 
