c1bc63ac306fff36b2fc81d2cf373227df5e3005
“Emmett <emmett.wechsler11@gmail.com>
2023-02-21 11:37:58 -0500

    trip backend branch fix

--- backend/routes/api/trips.js
+++ backend/routes/api/trips.js
@@ -4,36 +4,61 @@ const mongoose = require('mongoose');
 const Trip = mongoose.model('Trip');
 
 /* GET trips */
-// get all trips
+// get all trips by user
 router.get('/user/:userId', async function (req, res, next) {
     let user;
     const userId = req.params.userId;
     try {
-        user = await User.findById(req.params.userId);
-    } catch(err) {
-        const error = new Error('User not found');
-        error.statusCode = 404;
-        error.errors = { message: 'No user found with that id'};
-        return next(error);
-    }
-    try {
-        const trips = await Trip.find({ $or: [{organizer: userId}, { members: userId }]})
-                                .sort({ startDate: 1 });
+        const trips = await Trip.find({ $or: [{ organizer: userId }, { members: userId }] })
+            .sort({ startDate: 1 });
         return res.json(trips);
     }
-    catch(err) {
+    catch (err) {
         return res.json([]);
     }
 });
 
 // get trip by ID
-router.get('/:id', async function (req, res, next) {
-    res.json({
-        message: 'GET /api/trips/:id'
-    })
+router.get('/:tripId', async function (req, res, next) {
+    try {
+        const trip = await Trip.findById(req.params.tripId)
+            .populate("members", "_id name");
+        return res.json(trip);
+    }
+    catch (err) {
+        const error = new Error('Trip not found');
+        error.statusCode = 404;
+        error.errors = { message: "No trip found with that id" };
+        return next(error);
+    }
 });
 
 /* POST trip requests */
+
+// create a new trip
+router.post('/', async function (req, res, next) {
+    try {
+        const newTrip = new Trip({
+            title: req.body.title,
+            startDate: req.body.startDate,
+            endDate: req.body.endDate,
+            description: req.body.description,
+            location: req.body.location,
+            organizer: req.body.organizer,
+            members: req.body.members
+        })
+
+        let trip = newTrip.save();
+        return res.json(trip);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+/* Update trip listing */
+// update a trip
+
 
 
 module.exports = router;
