c9931ce92c558b87740540564648e80db3b11a91
“Shawna <shawna.e.hartley@gmail.com>
2023-02-21 13:44:58 -0500

    align form styling

--- frontend/src/components/TripCreateForm/TripCreateForm.js
+++ frontend/src/components/TripCreateForm/TripCreateForm.js
@@ -13,41 +13,41 @@ const TripCreateForm = () => {
 
       <div className='trip-create-content-wrapper'>
         <label className='trip-create-content-item'>
-          <h2 className='trip-create-content-title trip-title' > Trip title </h2>
+          <span className='trip-create-content-title trip-title' > Trip title </span>
           <input className='trip-create-content-input trip-title' type="text" placeholder='title'/>
         </label>
         <label className='trip-create-content-item'>
-          <h2 className='trip-create-content-title trip-description' > Trip description </h2>
+          <span className='trip-create-content-title trip-description' > Trip description </span>
           <input className='trip-create-content-input trip-description' type="text" placeholder='description'/>
         </label>
         <label className='trip-create-content-item'>
-          <h2 className='trip-create-content-title trip-start-date' > Trip start date </h2>
+          <span className='trip-create-content-title trip-start-date' > Trip start date </span>
           <input className='trip-create-content-input trip-start-date' type="date" placeholder='start date'/>
         </label>
         <label className='trip-create-content-item'>
-          <h2 className='trip-create-content-title trip-end-date' > Trip end date </h2>
+          <span className='trip-create-content-title trip-end-date' > Trip end date </span>
           <input className='trip-create-content-input trip-end-date' type="date" placeholder='end date'/>
         </label>
         <div className='trip-create-location-input-wrapper'>
-          <h1 className='trip-create-subtitle trip-location' > Trip location </h1>
+          <h2 className='trip-create-subtitle trip-location' > Trip location </h2>
           <label className='trip-create-content-item'>
-            <h2 className='trip-create-content-title trip-street-address'> Street address </h2>
+            <span className='trip-create-content-title trip-street-address'> Street address </span>
             <input className='trip-create-content-input trip-street-address' type="text" placeholder='street address'/>
           </label>
           <label className='trip-create-content-item'>
-            <h2 className='trip-create-content-title trip-city'> City </h2>
+            <span className='trip-create-content-title trip-city'> City </span>
             <input className='trip-create-content-input trip-city' type="text" placeholder='city'/>
           </label>
           <label className='trip-create-content-item'>
-            <h2 className='trip-create-content-title trip-state'> State </h2>
+            <span className='trip-create-content-title trip-state'> State </span>
             <input className='trip-create-content-input trip-state' type="text" placeholder='state'/>
           </label>
           <label className='trip-create-content-item'>
-            <h2 className='trip-create-content-title trip-zip-code'> Zip code </h2>
+            <span className='trip-create-content-title trip-zip-code'> Zip code </span>
             <input className='trip-create-content-input trip-zip-code' type="text" placeholder='zip code'/>
           </label>
           <label className='trip-create-content-item'>
-            <h2 className='trip-create-content-title trip-country'> Country </h2>
+            <span className='trip-create-content-title trip-country'> Country </span>
             <input className='trip-create-content-input trip-country' type="text" placeholder='country'/>
           </label>
         </div>
