cc6e4b9856e5648dc1c05d6e148914b06e732f56
“Shawna <shawna.e.hartley@gmail.com>
2023-02-20 21:30:53 -0500

    fix user auth errors and seed db

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -1,3 +1,11 @@
+const mongoose = require("mongoose");
+const { mongoURI: db } = require('../config/keys.js');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+
+console.log("I am in seeds")
+
 const users = [];
 users.push(
   new User ({
@@ -6,6 +14,8 @@ users.push(
     hashedPassword: bcrypt.hashSync('password', 10)
   })
 )
+
+console.log(users)
 
 users.push(
   new User ({
@@ -30,3 +40,30 @@ users.push(
     hashedPassword: bcrypt.hashSync('password', 10)
   })
 )
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+    insertSeeds();
+  })
+  .catch(err => {
+    console.log("yikes");
+    console.error(err.stack);
+    process.exit(1);
+  });
+
+const insertSeeds = () => {
+  console.log("Resetting db and seeding users and tweets...");
+  User.collection.drop()
+                .then(() => User.insertMany(users))
+                .then(() => {
+                  console.log("Done!");
+                  mongoose.disconnect();
+                })
+                .catch(err => {
+                  console.error(err.stack);
+                  process.exit(1);
+                });
+};
--- frontend/src/components/UserAuth/Signup.js
+++ frontend/src/components/UserAuth/Signup.js
@@ -1,22 +1,103 @@
-import React from 'React'; 
-import './UserAuth.css'; 
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import './UserAuth.css';
+import { signup, clearSessionErrors } from '../../store/session';
 
+function SignupForm () {
+const [email, setEmail] = useState('');
+const [name, setName] = useState('');
+const [password, setPassword] = useState('');
+const [password2, setPassword2] = useState('');
+const errors = useSelector(state => state.errors.session);
+const dispatch = useDispatch();
 
+useEffect(() => {
+    return () => {
+        dispatch(clearSessionErrors());
+    };}, [dispatch]);
 
-const SignupForm = () => {
+const update = field => {
+    let setState;
 
+    switch (field) {
+        case 'email':
+        setState = setEmail;
+        break;
+        case 'name':
+        setState = setName;
+        break;
+        case 'password':
+        setState = setPassword;
+        break;
+        case 'password2':
+        setState = setPassword2;
+        break;
+        default:
+        throw Error('Unknown field in Signup Form');
+    }
 
-    return (
-
-        <form action="">Signup Form placeholder
-
-            
-        </form>
-
-
-    )
-
+    return e => setState(e.currentTarget.value);
 }
 
+const handleSubmit = e => {
+    e.preventDefault();
+    const user = {
+        email,
+        name,
+        password
+    };
+
+    dispatch(signup(user)); 
+}
+
+return (
+<form className="session-form" onSubmit={handleSubmit}>
+    <h2>Sign Up Form</h2>
+    <div className="errors">{errors?.email}</div>
+    <label>
+        <span>Email</span>
+        <input type="text"
+            value={email}
+            onChange={update('email')}
+            placeholder="Email"
+        />
+    </label>
+    <div className="errors">{errors?.name}</div>
+    <label>
+        <span>Name</span>
+        <input type="text"
+            value={name}
+            onChange={update('name')}
+            placeholder="Name"
+        />
+    </label>
+    <div className="errors">{errors?.password}</div>
+    <label>
+        <span>Password</span>
+        <input type="password"
+            value={password}
+            onChange={update('password')}
+            placeholder="Password"
+        />
+    </label>
+    <div className="errors">
+    {password !== password2 && 'Confirm Password field must match'}
+    </div>
+    <label>
+        <span>Confirm Password</span>
+        <input type="password"
+            value={password2}
+            onChange={update('password2')}
+            placeholder="Confirm Password"
+        />
+    </label>
+    <input
+    type="submit"
+    value="Sign Up"
+    disabled={!email || !name || !password || password !== password2}
+    />
+</form>
+);
+}
 
 export default SignupForm;
