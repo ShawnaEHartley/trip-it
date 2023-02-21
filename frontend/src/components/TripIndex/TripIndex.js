421beeea9962b7b5260de14c8d84b157bf0e41a9
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-21 15:20:35 -0500

    Begin Trip Index

--- frontend/src/components/TripIndex/TripIndex.js
+++ frontend/src/components/TripIndex/TripIndex.js
@@ -1,14 +1,21 @@
 import React from 'react'; 
 import { useDispatch } from 'react-redux';
+import TripIndexItem from './TripIndexItem';
+import { useEffect } from 'react';
+import * as tripActions from '../../store/trips';
 
 
 const TripIndex = () => {
     const dispatch = useDispatch(); 
 
+    useEffect(() => {
+        dispatch(tripActions.fetchUserTrips())
+    })
+
 
     return (
         <div>
-            Trip index placeholder
+            
         </div>
 
     )
---
+++ frontend/src/components/TripIndex/TripIndexItem.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+
+
+const TripIndexItem = ({trip}) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default TripIndexItem;
