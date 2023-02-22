55a125cf7a3f2f8189abcd064afc7468e93bee83
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-21 23:15:38 -0500

    Main Page base skele. Stamp still needs work in ordering / style, as well as general clean-up for a cohesive feel

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -6,8 +6,47 @@ import './MainPage.css';
 const MainPage = () => {
 
     return (
-        <div>
-            MainPage Container placeholder
+        <div id='main-page-container'>
+            <div id='page'>
+                <div id='stamp-page-container'>
+                    <div id='stamp-page-header'>
+                        <h2>Trip Collection</h2>
+                        <p>Shawna Hartley</p>
+                    </div>
+                    <div className='stamp-container'>
+                        <div className='stamp'>
+                            <div className='link-replacement'>
+                                Trip name <br />
+                                Date and time <br />
+                                Friends
+                            </div>
+                        </div>
+                        <div className='stamp-divider' />
+                        <div className='stamp'>
+                            <div className='link-replacement'>
+                                Trip name <br />
+                                Date and time <br />
+                                Friends
+                            </div>
+                        </div>
+                        <div className='stamp-divider' />
+                        <div className='stamp'>
+                            <div className='link-replacement'>
+                                Trip name <br />
+                                Date and time <br />
+                                Friends
+                            </div>
+                        </div>
+                        <div className='stamp-divider' />
+                    </div>
+                    <div className='stamp-container'>
+
+                    </div>
+                    <div className='stamp-container'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
