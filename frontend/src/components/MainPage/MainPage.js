d7a3cb3581f91f4c73641f321e5553fa2ea2f067
Charles Tyra <charlestyra@MacBook-Pro-3.local>
2023-02-22 09:08:37 -0500

    Resolution for Main branch

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -6,48 +6,9 @@ import './MainPage.css';
 const MainPage = () => {
 
     return (
-        <div id='main-page-container'>
-            <div id='page'>
-                <div id='stamp-page-container'>
-                    <div id='stamp-page-header'>
-                        <h2>Trip Collection</h2>
-                        <p>Shawna Hartley</p>
-                    </div>
-                    <div className='stamp-container'>
-                        <div className='stamp'>
-                            <div className='link-replacement'>
-                                Trip name <br />
-                                Date and time <br />
-                                Friends
-                            </div>
-                        </div>
-                        <div className='stamp-divider' />
-                        <div className='stamp'>
-                            <div className='link-replacement'>
-                                Trip name <br />
-                                Date and time <br />
-                                Friends
-                            </div>
-                        </div>
-                        <div className='stamp-divider' />
-                        <div className='stamp'>
-                            <div className='link-replacement'>
-                                Trip name <br />
-                                Date and time <br />
-                                Friends
-                            </div>
-                        </div>
-                        <div className='stamp-divider' />
-                    </div>
-                    <div className='stamp-container'>
-
-                    </div>
-                    <div className='stamp-container'>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <TripIndex />
+        </>
     )
 
 };
