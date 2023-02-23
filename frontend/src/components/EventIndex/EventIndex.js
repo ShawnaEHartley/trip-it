f3dc2d872a9db82e35f8a7366ccbde32d9046773
Charles Tyra <charlestyra@MacBook-Pro-3.local>
2023-02-23 18:33:04 -0500

    Down to bar bones for event index item return

--- frontend/src/components/EventIndex/EventIndex.js
+++ frontend/src/components/EventIndex/EventIndex.js
@@ -24,41 +24,13 @@ const EventIndex = () => {
 
     console.log(events);
     return (
-        <> 
-            <div id='zig-zag11' className='pattern' />
-            <div className='trip-show-page-container'>
-
-                <div id='post-card-container'>
-                    <div className='trip-show-page-header-wrapper'>
-                        <div className='trip-show-page-header'>
-                        </div>
-                    </div>
-                    <div id='top-body-border'>
-                        <span>Events</span>
-                        <span>Details</span>
-                    </div>
-                    <div id='post-card-body-container'>
-                        <div className='post-card-space left-space'>
-                            {events.map((event) => {
-                                return (
-                                    <EventIndexItem event={event} />
-                                )
-                            })}
-                        </div>
-                        <div id='post-card-center-border' />
-                        <div className='post-card-space'>
-                            <div id='info-container'>
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id='post-card-bottom'>
-                    </div>
-                </div>
-            </div>
+        <>
+            {events.map((event) => {
+                return (
+                    <EventIndexItem event={event} />
+                )
+            })}
         </>
-
     )
 };
 
