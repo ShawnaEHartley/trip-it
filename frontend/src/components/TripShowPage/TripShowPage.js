5e0782c61a1a29b0d1831edf895427b0df3a4574
“Shawna <shawna.e.hartley@gmail.com>
2023-02-23 01:56:37 -0500

    map out trip flow

--- frontend/src/components/TripShowPage/TripShowPage.js
+++ frontend/src/components/TripShowPage/TripShowPage.js
@@ -1,8 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import TripUpdateForm from '../TripUpdateForm/TripUpdateForm';
 import InviteMemberForm from './InviteMemberForm';
+import {deleteTrip} from '../../store/trips'
 
 import './TripShowPage.css'
 
 const TripShowPage = ({trip}) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
 
 
   const inviteMember = (e) => {
@@ -16,12 +22,34 @@ const TripShowPage = ({trip}) => {
     // open modal to create an event form
   }
 
+  const tripOrganizerButtons = () => {
+    return (
+      <div>
+        <button onClick={updateTrip}>Update</button>
+        <button onClick={deleteThisTrip}>Delete</button>
+      </div>
+    )
+  };
+
+  const updateTrip = (e) => {
+    e.preventDefault();
+    // open modal to update trip form
+    <TripUpdateForm trip={trip}/>
+  };
+
+  const deleteThisTrip = (e) => {
+    e.preventDefault();
+    dispatch(deleteTrip(trip._id))
+  }
+
   return (
     <div className='trip-show-page-outter-wrapper'>
       <div className='trip-show-page-header-wrapper'>
         <div className='trip-show-page-header'>
           <div>{trip.title} </div>
           <div>{trip.organizer.name}</div>
+          { user === trip.organizer ? {tripOrganizerButtons} : ""}
+          <button></button>
           <button onClick={makeAnEvent}>Create an Event</button>
         </div>
       </div>
