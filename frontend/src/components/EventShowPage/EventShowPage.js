fcc968d67c6c6813c5d2394aa280bec451c996eb
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-23 23:27:17 -0500

    CSS Style for Event show page

--- frontend/src/components/EventShowPage/EventShowPage.js
+++ frontend/src/components/EventShowPage/EventShowPage.js
@@ -6,6 +6,10 @@ import * as eventActions from '../../store/events';
 import EventUpdateForm from '../EventsUpdateForm/EventsUpdateForm';
 import { closeModal } from '../../store/modal';
 import { useHistory } from 'react-router-dom';
+import emptyHeart from '../../assets/images/icons8-favorite-96.png';
+import heart from '../../assets/images/icons8-love-96.png'
+
+import './EventShowPage.css';
 
 const EventShowPage = () => {
 
@@ -14,6 +18,9 @@ const EventShowPage = () => {
   const event = useSelector(eventActions.getEvent);
   const user = useSelector((state) => state.session.user);
   const history = useHistory();
+
+  const [liked, setLiked] = useState(false);
+
 
   const modalState = useSelector((state) => {
     return state?.modal ? state.modal : null ;
@@ -64,38 +71,62 @@ const EventShowPage = () => {
   const eventOrganizer = event.peopleGoing[0]._id;
 
 
-  return (
-    <div className='event-show-page-wrapper'>
-      { modalState.on ? <div className='modal-background' onClick={()=> {dispatch(closeModal())}}></div> : "" }
-      { modalState.on ? <div className='modal-wrapper'> {modalComponent()}</div> : "" }
-      <div className='event-show-page-header-wrapper'>
-        <div className='event-show-page-header'>
-          <div>{event.title}</div>
-          <div>{event.startDate}</div>
-          <div>{event.endDate}</div>
-          { user._id === eventOrganizer ? eventOrganizerButtons() : ""}
-          <button>Like Event</button>
-        </div>
-      </div>
+  let splitStartDate = event.startDate.split('-');
+  let splitEndDate = event.endDate.split('-');
 
-      <div className='event-show-page-body-wrapper'>
-        <div className='event-show-page-body'>
-          <div>{event.description}</div>
-          <div>{ event.location.streetAddress ? event.location.streetAddress : "" }</div>
-          <div>{ event.location.city ? event.location.city : "" }</div>
-          <div>{ event.location.state ? event.location.state : "" }</div>
-          <div>{ event.location.zipCode ? event.location.zipCode : "" }</div>
-          <div>{ event.location.country ? event.location.country : "" }</div>
-          <div>Cost: ${event.cost} { event.splitCostStructure ? 'Per Person' : 'In Total'}</div>
-          <div>Booked? { event.booked ? 'yes' : 'no' }</div>
-          <div className='event-show-page-people-going'>
-            {event.peopleGoing.map((person) => {
-              return <div>{person.name}</div>
-            })}
+  const months = {
+    '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'June',
+    '07': 'July', '08': 'Aug', '09': 'Sept', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+  };
+
+  splitStartDate[1] = months[splitStartDate[1]];
+  splitEndDate[1] = months[splitEndDate[1]];
+  splitStartDate[2] = splitStartDate[2].split('T')[0];
+  splitEndDate[2] = splitEndDate[2].split('T')[0];
+
+  const monthStart = splitStartDate[1];
+  const monthEnd = splitEndDate[1];
+  const dayStart = splitStartDate[2];
+  const dayEnd = splitEndDate[2];
+  const yearStart = splitStartDate[0];
+  const yearEnd = splitEndDate[0];
+
+  return (
+    <>
+      <div id='zig-zag11' className='pattern'/>
+      <div className='event-show-page-wrapper'>
+        { modalState.on ? <div className='modal-background' onClick={()=> {dispatch(closeModal())}}></div> : "" }
+        { modalState.on ? <div className='modal-wrapper'> {modalComponent()}</div> : "" }
+        <div className='trip-show-page-container'>
+          {modalState.on ? <div className='modal-background' onClick={() => { dispatch(closeModal()) }}></div> : ""}
+          {modalState.on ? <div className='modal-wrapper'> {modalComponent()}</div> : ""}
+          <div id='post-card-container' className='striped-border'>
+            {liked ? <img id='heart' src={heart} onClick={() => setLiked(false)} /> : <img id='heart' src={emptyHeart} onClick={() => setLiked(true)} />}
+            <div className='trip-show-page-header-wrapper event-header'>
+              <div className='trip-show-page-header'>
+                <h2 id='event-header-h2'>{event.title}</h2>
+                <h4>{event.booked ? 'ALREADY BOOKED' : 'Not yet booked!'}</h4>
+              </div>
+            </div>
+            <div id='post-card-body-container'>
+              <div className='post-card-space left-space event-show-left'>
+                <p><span className='descr-span'>Cost: </span>${event.cost} {event.splitCostStructure ? 'per person' : 'total'}</p>
+                <p id='descr-description'>{event.description}</p>
+              </div>
+              <div id='event-post-card-center-border' />
+              <div className='post-card-space event-show-right'>
+                <p>{monthStart} {dayStart}, {yearStart} til {monthEnd} {dayEnd}, {yearEnd}</p>
+                  <div id='location-div'>
+                    <p>{event.location.name ? event.location.name : ""}</p>
+                    <p>{event.location.city ? event.location.city : ""}</p>
+                    <p>{event.location.country ? event.location.country : ""} {event.location.zipCode ? event.location.zipCode : ""}</p>
+                  </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 };
 
