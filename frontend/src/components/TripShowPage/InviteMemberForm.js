84fd1a6f6437f82b2adc26fd5ea4787165a089f6
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-03-08 12:12:20 -0500

    Prevent users from being added to trip more than once

--- frontend/src/components/TripShowPage/InviteMemberForm.js
+++ frontend/src/components/TripShowPage/InviteMemberForm.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import { addUserToTrip, fetchTrip } from '../../store/trips';
+import { addUserToTrip, fetchTrip, tripErrorsReducer } from '../../store/trips';
 
 import './InviteMemberForm.css';
 
-const InviteMemberForm = (trip) => {
+const InviteMemberForm = ({trip}) => {
   const dispatch = useDispatch();
   const { tripId } = useParams();
+  const attendees = trip.members
+  const errors = useSelector(state => state.errors.tripErrorsReducer);
+
 
   useEffect(() => {
     dispatch(fetchTrip(tripId));
@@ -16,22 +19,31 @@ const InviteMemberForm = (trip) => {
 
   // search member by email
 
-  const [addMember, setaddMember] = useState("");
+  const [inviteeEmail, setInviteeEmail] = useState("");
 
   const inviteMember = (e) => {
     e.preventDefault();
     //invite a member to this trip via their email via modal
-    dispatch(addUserToTrip(tripId, addMember))
-  };
 
-  console.log(addMember)
+    console.log(attendees)
+    console.log(inviteeEmail)
+    const includes = attendees.some(member => member.email === inviteeEmail);
+
+    if (includes) {
+      console.log('Email exists')
+    } else {
+      // dispatch(addUserToTrip(tripId, inviteeEmail))
+      console.log('Email does not exist')
+    }
+  };
 
   return (
     <form action="">
       <div>Enter the email</div>
-      <input type="text" value= {addMember} onChange={e => {
+      <div></div>
+      <input type="text" value= {inviteeEmail} onChange={e => {
         e.preventDefault();
-        setaddMember(e.target.value)
+        setInviteeEmail(e.target.value)
       }} />
       <button onClick={inviteMember}>Invite</button>
     </form>
